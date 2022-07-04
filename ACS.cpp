0165f64d8e59b97d448a0987130044bf6c86edb1
OmkarNarvekar001 <onarvekar01@gmail.com>
2022-07-04 14:24:05 +0530

    Cout the AC's

---
+++ ACS.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int problem(int &p){
+    int a = p/100;
+    int b= p%100;
+    if((a+b)<=10){
+        return a+b;
+    }
+    else{
+        return -1;
+    }
+    
+}
+
+int main() {
+	int t ;
+	cin>>t;
+	while(t--){
+	    int p;
+	    cin>>p;
+	    int res = problem(p);
+	    cout<<res<<endl;
+	}
+	return 0;
+}
+
+
